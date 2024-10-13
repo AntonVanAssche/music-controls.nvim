63153444ef52fefbc06cfdfadc572d1a3c17a747
Anton <vanasscheanton@gmail.com>
2024-10-13 16:50:30 +0200

    [play] Add error handling
    - Implemented `pcall` to handle potential errors when toggling
      play/pause state.
    - Added an error message if the play/pause command fails.
    - Improved error reporting and stability for the play/pause
      functionality.

--- lua/music-controls/cmds/play.lua
+++ lua/music-controls/cmds/play.lua
@@ -11,7 +11,10 @@ M.play = function(player)
   end
 
   local cmd = string.format('playerctl -p %s play-pause', player)
-  utils.exec_command(cmd)
+  local success, _ = pcall(utils.exec_command, cmd)
+  if not success then
+    return 'Failed to toggle play/pause', 'error', { title = 'Music Controls' }
+  end
 
   utils.sleep(0.25)
   return require('music-controls.cmds.current').current(player)
