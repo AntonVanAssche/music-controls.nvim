b29e1926ab2492259a15a2a7033a1ed7e35b8426
Anton Van Assche <vanasscheanton@gmail.com>
2024-12-16 19:55:21 +0100

    Prevent plugin from loading without playerctl

--- plugin/music-controls.lua
+++ plugin/music-controls.lua
@@ -2,6 +2,11 @@ if vim.g.loaded_music_controls then
   return
 end
 
+if vim.fn.executable('playerctl') == 0 then
+  vim.api.nvim_err_writeln('MusicControls: playerctl is not installed')
+  return
+end
+
 require('music-controls').setup()
 require('music-controls.commands').setup()
 
